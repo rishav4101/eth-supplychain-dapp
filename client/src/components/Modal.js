81753a126d42d050dde03929a0715ebf11a7d3ef
AbhinavKrishna26 <abhinavkrishna26chemistry@gmail.com>
2021-05-29 03:11:11 +0530

    [Added]Modal

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,38 +1,60 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Modal from '@material-ui/core/Modal';
-import Backdrop from '@material-ui/core/Backdrop';
-import Fade from '@material-ui/core/Fade';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 
 const useStyles = makeStyles((theme) => ({
   modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
   },
   paper: {
     backgroundColor: theme.palette.background.paper,
-    border: '0px solid #000',
+    border: "0px solid #000",
     boxShadow: theme.shadows[5],
     padding: theme.spacing(2, 4, 3),
-    outline:"none"
+    outline: "none",
+  },
+  dRow: {
+    width: "100%",
+    borderBottom: `1px solid #222`,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+    maxWidth: "550px",
+    margin: "0 auto",
+  },
+
+  dCol: {
+    width: "50%",
+    textAlign: "left",
+    fontWeight: 600,
+    color: "#1a237e",
   },
 }));
 
-export default function ProductModal({prod, open, handleClose, handleReceiveButton}) {
-    const [rdata, setRdata] = React.useState({
-        long : "",
-        lat: ""
-    })
+export default function ProductModal({
+  prod,
+  open,
+  handleClose,
+  handleReceiveButton,
+}) {
+  const [rdata, setRdata] = React.useState({
+    long: "",
+    lat: "",
+  });
 
-    const handleChangeForm = async (e) => {
-        setRdata({
-            ...rdata,
-            [e.target.name] : e.target.value
-        })
-    }
+  const handleChangeForm = async (e) => {
+    setRdata({
+      ...rdata,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   const classes = useStyles();
   return (
@@ -51,45 +73,146 @@ export default function ProductModal({prod, open, handleClose, handleReceiveButt
       >
         <Fade in={open}>
           <div className={classes.paper}>
-        {prod.length === 0 ? <></> : (<div>
-                <p>Universal ID : {prod[0][0]}</p>
-                <p>SKU : {prod[0][1]}</p>
-                <p>Owner : {prod[0][2]}</p>
-                <p>Manufacturer : {prod[0][3]}</p>
-                <p>Name of Manufacturer : {prod[0][4]}</p>
-                <p>Details of Manufacturer : {prod[0][5]}</p>
-                <p>Longitude of Manufature : {prod[0][6]}</p>
-                <p>Latitude of Manufature : {prod[0][7]}</p>
+            {prod.length === 0 ? (
+              <></>
+            ) : (
+              <div style={{ maxHeight: "450px", overflow: "scroll" }}>
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>Universal ID : </div>
+                  <div className={classes.dCol}>{prod[0][0]}</div>
+                </div>
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>SKU :</div>{" "}
+                  <div className={classes.dCol}> {prod[0][1]}</div>
+                </div>
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>Owner : </div>{" "}
+                  <div className={classes.dCol}>{prod[0][2]}</div>
+                </div>
 
-                <p>Manufactured date : {prod[1][0]}</p>
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>Manufacturer :</div>{" "}
+                  <div className={classes.dCol}>{prod[0][3]}</div>
+                </div>
 
-{prod[1][5] == "2" || prod[1][5] == "5" ? <>
-                <TextField
-        name="long"
-        variant="outlined"
-        value={rdata.long}
-        onChange={handleChangeForm}
-        label="long"
-        />
-        <TextField
-        name="lat"
-        variant="outlined"
-        value={rdata.lat}
-        onChange={handleChangeForm}
-        label="lat"
-        />
-</> : <> </> }
-{prod[1][5] === "2" || prod[1][5] === "5" || prod[1][5] === "7"? <>
-                <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            onClick={() => handleReceiveButton(prod[0][0], rdata.long, rdata.lat)}
-        >
-            Recieve
-        </Button>
-        </> : <> </> }
-        </div>)}
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>Name of Manufacturer :</div>{" "}
+                  <div className={classes.dCol}> {prod[0][4]}</div>
+                </div>
+
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>Manufactured date :</div>{" "}
+                  <div className={classes.dCol}> {prod[1][0]}</div>
+                </div>
+
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>Details of Manufacturer :</div>{" "}
+                  <div className={classes.dCol}> {prod[0][5]}</div>
+                </div>
+
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>Longitude of Manufature : </div>{" "}
+                  <div className={classes.dCol}>{prod[0][6]}</div>
+                </div>
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>Latitude of Manufature :</div>{" "}
+                  <div className={classes.dCol}>{prod[0][7]}</div>
+                </div>
+
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>Product Name : </div>{" "}
+                  <div className={classes.dCol}>{prod[1][1]}</div>
+                </div>
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>Product Code:</div>{" "}
+                  <div className={classes.dCol}>{prod[1][2]}</div>
+                </div>
+
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>Product Price: </div>{" "}
+                  <div className={classes.dCol}>{prod[1][3]}</div>
+                </div>
+
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}> Product Category: </div>
+                  <div className={classes.dCol}>{prod[1][4]}</div>
+                </div>
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>Product State: </div>{" "}
+                  <div className={classes.dCol}>{prod[1][5]}</div>
+                </div>
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>Third Party Address: </div>{" "}
+                  <div className={classes.dCol}>{prod[1][6]}</div>
+                </div>
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>
+                    Third Party Longitude: </div> <div className={classes.dCol}>{prod[1][7]}
+                  </div>
+                </div>
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>
+                    Third Party Latitude: </div> <div className={classes.dCol}>{prod[2][0]}
+                  </div>
+                </div>
+
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>Delivery Hub Address:</div>{" "}
+                  <div className={classes.dCol}> {prod[2][1]}</div>
+                </div>
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>Delivery Hub Longitude: </div>{" "}
+                  <div className={classes.dCol}>{prod[2][2]}</div>
+                </div>
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>Delivery Hub Latitude:</div>{" "}
+                  <div className={classes.dCol}> {prod[2][3]}</div>
+                </div>
+                <div className={classes.dRow}>
+                  <div className={classes.dCol}>Customer Address: </div>{" "}
+                  <div className={classes.dCol}>{prod[2][4]}</div>
+                </div>
+
+                {prod[1][5] == "2" || prod[1][5] == "5" ? (
+                  <>
+                    <TextField
+                      name="long"
+                      variant="outlined"
+                      value={rdata.long}
+                      onChange={handleChangeForm}
+                      label="long"
+                    />
+                    <TextField
+                      name="lat"
+                      variant="outlined"
+                      value={rdata.lat}
+                      onChange={handleChangeForm}
+                      label="lat"
+                    />
+                  </>
+                ) : (
+                  <> </>
+                )}
+                {prod[1][5] === "2" ||
+                prod[1][5] === "5" ||
+                prod[1][5] === "7" ? (
+                  <>
+                    <Button
+                      type="submit"
+                      variant="contained"
+                      color="primary"
+                      onClick={() =>
+                        handleReceiveButton(prod[0][0], rdata.long, rdata.lat)
+                      }
+                    >
+                      Recieve
+                    </Button>
+                  </>
+                ) : (
+                  <> </>
+                )}
+              </div>
+            )}
           </div>
         </Fade>
       </Modal>
