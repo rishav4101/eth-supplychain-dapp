dd48849b3adecfecf3986448e722d16abee092ea
rishav4101 <rajkumarrishav4101@gmail.com>
2021-05-29 02:20:53 +0530

    [Styled] Receive pages

--- client/src/pages/DeliveryHub/ReceiveDeliveryHub.js
+++ client/src/pages/DeliveryHub/ReceiveDeliveryHub.js
@@ -3,6 +3,16 @@ import Navbar from "../../components/Navbar";
 import Button from "@material-ui/core/Button";
 import ProductModal from "../../components/Modal";
 import { useRole } from "../../context/RoleDataContext";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import TablePagination from "@material-ui/core/TablePagination";
+import { useStyles } from "../../components/Styles";
+import clsx from "clsx";
 
 export default function ReceiveDeliveryHub(props){
     const accounts = props.accounts;
@@ -12,6 +22,7 @@ export default function ReceiveDeliveryHub(props){
     const [allReceiveProducts, setAllReceiveProducts] = React.useState([]);
     const [modalData, setModalData] = useState([]);
     const [open, setOpen] = useState(false);
+    const classes = useStyles();
 
     React.useEffect(() => {
         (async () => {
@@ -22,18 +33,30 @@ export default function ReceiveDeliveryHub(props){
 
         (async () => {
             const arr = [];
-            for(var i = 1; i<count; i++){
-                var a = await supplyChainContract.methods.fetchProductPart1(i, 'product', 0).call();
-                var b = await supplyChainContract.methods.fetchProductPart2(i, 'product', 0).call();
-                if(b[5] == "5"){
-                    const ar = [];
-                    ar.push(a); ar.push(b);
-                    arr.push(ar);
-                }
+            for (var i = 1; i < count; i++) {
+              const prodState = await supplyChainContract.methods
+                .fetchProductState(i)
+                .call();
+      
+              if (prodState === "5") {
+                const prodData = [];
+                const a = await supplyChainContract.methods
+                  .fetchProductPart1(i, "product", 0)
+                  .call();
+                const b = await supplyChainContract.methods
+                  .fetchProductPart2(i, "product", 0)
+                  .call();
+                const c = await supplyChainContract.methods
+                  .fetchProductPart2(i, "product", 0)
+                  .call();
+                prodData.push(a);
+                prodData.push(b);
+                prodData.push(c);
+                arr.push(prodData);
+              }
             }
-            await setAllReceiveProducts(arr);
-            
-            }) ();
+            setAllReceiveProducts(arr);
+          })();
 
     }, [count])
 
@@ -42,6 +65,19 @@ export default function ReceiveDeliveryHub(props){
         setCount(0);
         setOpen(false);
     }
+
+    const [page, setPage] = React.useState(0);
+        const [rowsPerPage, setRowsPerPage] = React.useState(10);
+      
+        const handleChangePage = (event, newPage) => {
+          setPage(newPage);
+        };
+      
+        const handleChangeRowsPerPage = (event) => {
+          setRowsPerPage(+event.target.value);
+          setPage(0);
+        };
+
 
     const handleClose = () => setOpen(false);
 
@@ -52,13 +88,143 @@ export default function ReceiveDeliveryHub(props){
     };
 
     return(
-        <>
+        <div classname={classes.pageWrap}>
         <Navbar>
         <ProductModal prod={modalData} open={open} handleClose={handleClose} handleReceiveButton={handleReceiveButton} />
 
-        <h1>All Products To be Receive</h1>
-        <h2>Total : {count}</h2>
-          {allReceiveProducts.length !== 0 ? (allReceiveProducts.map((prod) => (
+        <h1 className={classes.pageHeading}>All Products To be Receive</h1>
+        <h3 className={classes.tableCount}>Total : {allReceiveProducts.length}</h3>
+
+        <div>
+          <Paper className={classes.TableRoot}>
+            <TableContainer className={classes.TableContainer}>
+              <Table stickyHeader aria-label="sticky table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell className={classes.TableHead} align="left">
+                      Universal ID
+                    </TableCell>
+                    <TableCell className={classes.TableHead} align="center">
+                      Product Code
+                    </TableCell>
+                    <TableCell className={classes.TableHead} align="center">
+                      Manufacturer
+                    </TableCell>
+                    <TableCell className={classes.TableHead} align="center">
+                      Manufacture Date
+                    </TableCell>
+                    <TableCell className={classes.TableHead} align="center">
+                      Product Name
+                    </TableCell>
+                    <TableCell
+                      className={clsx(classes.TableHead, classes.AddressCell)}
+                      align="center"
+                    >
+                      Owner
+                    </TableCell>
+                    <TableCell
+                      className={clsx(classes.TableHead)}
+                      align="center"
+                    >
+                      RECEIVE
+                    </TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {allReceiveProducts.length !== 0 ? (
+                    allReceiveProducts
+                      .slice(
+                        page * rowsPerPage,
+                        page * rowsPerPage + rowsPerPage
+                      )
+                      .map((prod) => {
+                        return (
+                            <>
+                          <TableRow
+                            hover
+                            role="checkbox"
+                            tabIndex={-1}
+                            key={prod[0][0]}
+                            
+                          >
+                            <TableCell
+                              className={classes.TableCell}
+                              component="th"
+                              align="left"
+                              scope="row"
+                              onClick={() => handleClick(prod)}
+                            >
+                              {prod[0][0]}
+                            </TableCell>
+                            <TableCell
+                              className={classes.TableCell}
+                              align="center"
+                              onClick={() => handleClick(prod)}
+                            >
+                              {prod[1][2]}
+                            </TableCell>
+                            <TableCell
+                              className={classes.TableCell}
+                              align="center"
+                              onClick={() => handleClick(prod)}
+                            >
+                              {prod[0][4]}
+                            </TableCell>
+                            <TableCell align="center" onClick={() => handleClick(prod)}>{prod[1][0]}</TableCell>
+                            <TableCell
+                              className={classes.TableCell}
+                              align="center"
+                              onClick={() => handleClick(prod)}
+                            >
+                              {prod[1][1]}
+                            </TableCell>
+                            <TableCell
+                              className={clsx(
+                                classes.TableCell,
+                                classes.AddressCell
+                              )}
+                              align="center"
+                              onClick={() => handleClick(prod)}
+                            >
+                              {prod[0][2]}
+                            </TableCell>
+                            <TableCell
+                           className={clsx(classes.TableCell)}
+                           align="center"
+                         >
+                           <Button
+                             type="submit"
+                             variant="contained"
+                             color="primary"
+                             onClick={() => handleClick(prod)}
+                           >
+                             RECEIVE
+                           </Button>
+                         </TableCell>
+                          </TableRow>
+                           
+                         </>
+                        );
+                      })
+                  ) : (
+                    <> </>
+                  )}
+                </TableBody>
+              </Table>
+            </TableContainer>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 100]}
+              component="div"
+              count={allReceiveProducts.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onChangePage={handleChangePage}
+              onChangeRowsPerPage={handleChangeRowsPerPage}
+            />
+          </Paper>
+        </div>
+
+          {/* {allReceiveProducts.length !== 0 ? (allReceiveProducts.map((prod) => (
                 <>
                     <div>
                     <p>Universal ID : {prod[0][0]}</p>
@@ -82,8 +248,8 @@ export default function ReceiveDeliveryHub(props){
                     </div>
                     
                 </>
-          ))) : <> </>}
+          ))) : <> </>} */}
           </Navbar>
-        </>
+        </div>
     )
 }
