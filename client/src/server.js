8821080b6c993f906a4bf1a109e7b4cca2fefd0f
error <ramiz.mollah07@outlook.com>
2021-05-30 00:12:12 +0530

    added express server for prod

---
+++ client/src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+// const server = http.createServer(app);
+
+const port =  3000;
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port);
+});
