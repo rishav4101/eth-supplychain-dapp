d63f72127db18e762146835effcd2c284acc4c85
error <ramiz.mollah07@outlook.com>
2021-05-30 00:28:29 +0530

    [ADDED] exress for serving builds

--- client/src/server.js
+++ client/src/server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname,'..', 'build')));
 
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname,'..', 'build', 'index.html'));
 });
 
 
